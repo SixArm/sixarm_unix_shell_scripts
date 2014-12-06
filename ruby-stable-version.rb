002cf44a0dc274f8172bd19334b80e2e0ba73f78
SixArm <sixarm@sixarm.com>
2014-12-05 19:38:49 -0700

    Streamline

--- ruby-stable-version.rb
+++ ruby-stable-version.rb
@@ -10,6 +10,9 @@
 # This implementation fetches the Ruby website download page,
 # and parses the result to find the file download link and version.
 #
+# Author: Joel Parker Henderson (joel@joelparkerhenderson.com)
+# License: GPL
+#
 require 'net/http'
 txt = Net::HTTP.get(URI("https://www.ruby-lang.org/en/downloads/"))
-ver = txt[/cache.ruby-lang.org.*?ruby-(.*?).tar.gz/,1]
+ver = txt[/\/ruby-(.*?).tar.gz/,1]
