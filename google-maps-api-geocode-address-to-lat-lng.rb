9dbe7dbfeefab5d1b38f47ca3688b5fa9d505b1b
Joel Parker Henderson <joel@joelparkerhenderson.com>
2018-01-11 20:39:29 -0800

    Upgrade Google Maps API scripts

--- google-maps-api-geocode-address-to-lat-lng.rb
+++ google-maps-api-geocode-address-to-lat-lng.rb
@@ -1,8 +1,8 @@
 #!/usr/bin/env ruby
 
 ##
-# Ruby command line script to geocode
-# from an address to latitude and longitude.
+# Google Maps API ruby command line script to geocode
+# from an arbritray address to latitude and longitude
 #
 # Example:
 #
@@ -12,24 +12,46 @@
 # This script uses the free Google geocoding API here:
 # http://code.google.com/apis/maps/documentation/geocoding/
 #
-# Contact: Joel Parker Henderson, joel@joelparkerhenderson.com
-# License: Creative Commons Share And Share Alike
-# Version: 1.0.0
+# You may want to compare these two implemenations:
+#
+#  * google-maps-api-geocode-address-to-lat-lng.rb (Ruby)
+#  * google-maps-api-geocode-address-to-lat-lng.sh (Shell)
+#
+#
+# ## Tracking
+#
+#   * Program: google-maps-api-geocode-address-to-lat-lng.rb
+#   * Version: 2.0.0
+#   * Created: 2011-01-25
+#   * Updated: 2018-01-11
+#   * License: GPL
+#   * Contact: Joel Parker Henderson (joel@joelparkerhenderson.com)
 ##
 
 require 'net/http'
 require 'cgi'
 require 'json'
 
+google_maps_api_key=ENV["GOOGLE_MAPS_API_KEY"]
 address = ARGV.shift
 
+# Tweak Ruby's built-in HTTP module so it can handle HTTPS GET
+def https_get_uri(uri)
+  http = Net::HTTP.new(uri.host, uri.port)
+  http.use_ssl = true
+  http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+  request = Net::HTTP::Get.new(uri.request_uri)
+  response = http.get(request)
+  return response
+end
+  
 begin
 
   # Google provides a geocoder web service that can process an address into a location.
-  geocoder = "http://maps.googleapis.com/maps/api/geocode/json?sensor=false&address="
+  geocoder = "https://maps.googleapis.com/maps/api/geocode/json"
 
   # Prepare the freeform address text into a valid web URI.
-  href = geocoder + CGI.escape(address)
+  href = geocoder + "?" + "key=" + CGI.escape(google_maps_api_key) + "&" + "address=" + CGI.escape(address)
   uri = URI.parse(href)
   
   # Connect to Google API.
@@ -54,4 +76,3 @@ rescue Exception => e
   # Print the error message to STDERR.
   $stderr.puts "#{e.message} for address: #{address}"
 end 
-
---
+++ google-maps-api-geocode-address-to-lat-lng.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+
+## 
+# Google Maps API shell script to geocode from an
+# an arbitrary address to a latitude and longitude
+#
+# Syntax:
+#
+#     google-maps-api-geocode-address-to-lat-lng <address> 
+#
+# Example:
+#
+#     $ export GOOGLE_MAPS_API_KEY=abcdefghijklm
+#     $ google-maps-api-geocode-address-to-lat-lng "1 Main St, San Francisco CA 94111" 
+#     37.7931108 -122.3964898
+#
+# This script uses the free Google geocoding API here:
+# http://code.google.com/apis/maps/documentation/geocoding/
+#
+# You may want to compare these two implemenations:
+#
+#  * google-maps-api-geocode-address-to-lat-lng.rb (Ruby)
+#  * google-maps-api-geocode-address-to-lat-lng.sh (Shell)
+#
+#
+# ## Tracking
+#
+#   * Command: google-maps-api-geocode-address-to-lat-lng.sh
+#   * Version: 2.0.0
+#   * Created: 2011-01-25
+#   * Updated: 2018-01-11
+#   * License: GPL
+#   * Contact: Joel Parker Henderson (joel@joelparkerhenderson.com)
+##
+
+set -euf
+. sixarm-shell-functions
+
+CURL=${CURL:-curl}; cmd "$CURL" || die_cmd "$CURL"
+JQ=${JQ:-jq}; cmd "$JQ" || die_cmd "$JQ"
+
+## Args
+address="${1:-}"
+
+## Preflight
+[ -z ${GOOGLE_MAPS_API_KEY+x} ] && die_var GOOGLE_MAPS_API_KEY
+[ -z "$address" ] && die_var address
+
+## Main
+"$CURL" -sSLG "https://maps.googleapis.com/maps/api/geocode/json" \
+  --data-urlencode "key=$GOOGLE_MAPS_API_KEY" \
+  --data-urlencode "address=$address" |
+"$JQ" -r ".results[0].geometry.location | \"\(.lat) \(.lng)\""
+
