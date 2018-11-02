cb79a11ae1deb381be320532772552f4668a33c9
Joel Parker Henderson <joel@joelparkerhenderson.com>
2018-11-02 09:28:01 -0700

    Add shared client

--- asana-api-demo.py
+++ asana-api-demo.py
@@ -54,15 +54,12 @@ def asana_personal_access_token():
     """Read the Asasa personal access token via the environment."""
     x = os.getenv("ASANA_PERSONAL_ACCESS_TOKEN")
     if x is None:
-        print("This script needs an Asana personal access token.", file=sys.stderr)
+        print("This script needs an environment variable ASANA_PERSONAL_ACCESS_TOKEN.", file=sys.stderr)
         quit()
     return x
 
-# Construct an Asana client.
-client = asana.Client.access_token(token)
-
-# Set things up to send the name of this script to us to show that you succeeded! This is optional.
-client.options['client_name'] = "asana-api-demo"
+def asana_client():
+    return asana.Client.access_token(asana_personal_access_token())
 
 ##
 #
@@ -70,57 +67,75 @@ client.options['client_name'] = "asana-api-demo"
 #
 ##
 
-print("Asana API demo")
-
 def show_me():
     print("Show me...")
-    me = client.users.me()
+    me = ASANA_CLIENT.users.me()
     print(f"My name:{me['name']}")
 
 def show_workspaces():
     print("Show workspaces...")
-    for workspace in client.workspaces.find_all():
+    for workspace in ASANA_CLIENT.workspaces.find_all():
         print(f"workspace name:{workspace['name']}")
 
 def show_workspaces_and_projects():
     print("Show each workspace and its projects...")
-    for workspace in client.workspaces.find_all():
+    for workspace in ASANA_CLIENT.workspaces.find_all():
         print(f"workspace name:{workspace['name']}")
-        for project in client.projects.find_by_workspace(workspace['id']):
+        for project in ASANA_CLIENT.projects.find_by_workspace(workspace['id']):
             print(f"project name: {project['name']}")
 
 def show_workspaces_and_projects_and_tasks():
     print("Show each workspace and each project and its tasks")
-    for workspace in client.workspaces.find_all():
+    for workspace in ASANA_CLIENT.workspaces.find_all():
        print(f"workspace name:{workspace['name']}")
-       for project in client.projects.find_by_workspace(workspace['id']):
+       for project in ASANA_CLIENT.projects.find_by_workspace(workspace['id']):
            print(f"project name: {project['name']}")
-           for task in client.tasks.find_by_project(project['id']):
+           for task in ASANA_CLIENT.tasks.find_by_project(project['id']):
                print(f"task name: {task['name']}")
-    
+
+def show_tags():
+    print("Show each workspace and its tags")
+    for workspace in ASANA_CLIENT.workspaces.find_all():
+       print(f"workspace name:{workspace['name']}")
+       for tag in ASANA_CLIENT.tags.find_all({'workspace': workspace['id']}):
+            print(f"tag name:{tag['name']}")
+        
 def create_task():
     print("Create a task in the first workspace and first project.")
     
-    workspace = list(client.workspaces.find_all())[0]
+    workspace = list(ASANA_CLIENT.workspaces.find_all())[0]
     print(f"workspace:{workspace['name']}")
 
-    project = list(client.projects.find_all({'workspace': workspace['id']}))[0]
+    project = list(ASANA_CLIENT.projects.find_all({'workspace': workspace['id']}))[0]
     print(f"project:{project['name']}")
 
     name = "Demo task name"
     notes = "Demo task notes"
 
-    result = client.tasks.create_in_workspace(
+    task = ASANA_CLIENT.tasks.create_in_workspace(
         workspace['id'],
         {'name': name,
          'notes': notes,
          'projects': [project['id']]
         })
-
     pprint(result)
+    
+    task_id = ta['id']
+    print(f"task id:{task_id}")
+
+    task = ASANA_CLIENT.tasks.find_by_id(task_id)
+    pprint(task)
+
+ASANA_CLIENT = asana_client()
 
 def main():
-    create_task()
+    print("Asana API demo")
+    #show_me()
+    #show_workspaces()
+    show_workspaces_and_projects()
+    #show_workspaces_and_projects_and_tasks()    
+    show_tags()
+    #create_task()
   
 if __name__== "__main__":
     main()
